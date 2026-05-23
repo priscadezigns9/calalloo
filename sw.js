64a70d6860d824f14489041215c43754ff74e922
Prisca Dezigns <priscadezigns9@gmail.com>
2026-05-23 15:42:39 -0400

    feat: Service Worker

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = 'calalloo-v1';
+const ASSETS = ['/login/', '/dashboard/', '/index.html'];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
