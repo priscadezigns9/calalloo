a23db5f1360088284c4aa9abf238986520296bb9
Prisca Dezigns <priscadezigns9@gmail.com>
2026-05-23 18:49:56 -0400

    PWA: robust service worker strategy

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'calalloo-pwa-v2';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/logo.svg',
+  '/icon.svg',
+  '/manifest.json',
+  '/login/',
+  '/dashboard/',
+  '/about/',
+  '/magazine/'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
