8e10ae6bf25eb85b2a44a8eceb6822e4a9e95e4e
Prisca Dezigns <priscadezigns9@gmail.com>
2026-05-29 21:02:11 -0400

    FIX: Restore generateId function for authentication

--- js/backend.js
+++ js/backend.js
@@ -19,6 +19,17 @@ const supabase = {
     },
 
     auth: {
+        generateId() {
+            try {
+                if (typeof crypto !== 'undefined' && typeof crypto.randomUUID === 'function') {
+                    return crypto.randomUUID();
+                }
+            } catch (e) {}
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+                return v.toString(16);
+            });
+        },
         loadProfile() {
             const user = this.getUser();
             if (!user) return;
