0dd8ad5aa08c2e55ccb62be10a32924c524e910d
Prisca Dezigns <priscadezigns9@gmail.com>
2026-06-03 07:18:43 -0400

    v35.0: Update Service Worker

--- sw.js
+++ sw.js
@@ -1,18 +1,14 @@
+const CACHE_NAME = 'calalloo-v1';
+const ASSETS = [
+  '/dashboard/index.html',
+  '/assets/logo.png',
+  'https://fonts.googleapis.com/css2?family=Fraunces:opsz,ital,wght@9..144,0,700;9..144,0,900&family=Inter:wght@400;500;600;700;800&display=swap'
+];
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('calalloo-v1').then((cache) => {
-      return cache.addAll([
-        '/dashboard/',
-        'https://fonts.googleapis.com/css2?family=Fraunces:opsz,ital,wght@9..144,0,700;9..144,0,900;9..144,1,900&family=Inter:wght@400;500;600;700;800&display=swap'
-      ]);
-    })
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
